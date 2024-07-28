be4094bb80238d865b7cd27c19d4c11a1ba23c2d
aoshingbesan <a.oshingbes@alustudent.com>
2024-07-29 00:43:28 +0200

    fetched signedup user

--- script.js
+++ script.js
@@ -55,3 +55,41 @@ document.addEventListener('DOMContentLoaded', function () {
         confirmation.classList.add('hidden');
     });
 });
+
+const supabaseUrl = 'https://cgwfbohodsudyhyuuxwa.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnd2Zib2hvZHN1ZHloeXV1eHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwNjU1NzYsImV4cCI6MjAzNzY0MTU3Nn0.oLlmLnpGf09Edemcw8k8XanluU8wehFmTQ44HSg3dtM';
+const client = supabase.createClient(supabaseUrl, supabaseKey);
+
+document.addEventListener('DOMContentLoaded', async function () {
+    // Get the user details from Supabase on authentication
+    const { data: { user } } = await client.auth.getUser();
+
+    console.log(user);
+
+    // Function to update the welcome message with the user's full name
+    function updateWelcomeMessage(userName) {
+        const welcomeMessageElement = document.querySelector('.welcome-message h1');
+        // Update the welcome message with the user's full name
+        welcomeMessageElement.textContent = `Welcome, ${userName}`;
+    }
+    
+    function updateprofilename(userName) {
+        // Target the h1 element within the profile-info class inside profile-card
+        const profileNameElement = document.querySelector('.profile-card .profile-info h1');
+        // Update the profile name with the user's full name
+        profileNameElement.textContent = userName;
+    }
+
+
+
+
+    // Check if the user object exists and has the full_name property
+    if (user && user.user_metadata.first_name) {
+        // updateWelcomeMessage("Ademola");
+        updateWelcomeMessage(user.user_metadata.first_name);
+        updateprofilename(`${user.user_metadata.first_name} ${user.user_metadata.last_name}`)
+    } else {
+        console.error('User details not found');
+    }
+
+});
